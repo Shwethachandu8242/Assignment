4f18ecc4b9cfb5d10f0fc269db0840c117e3450f
Shwethachandu8242 <113550294+Shwethachandu8242@users.noreply.github.com>
2022-09-26 23:41:41 +0530

    java

---
+++ ToothPaste.java
@@ -0,0 +1,129 @@
+class ToothPaste
+{
+	static String companyName;
+	static String ProductHighlights;
+	static String ModelName;
+	static String Ideal_For;
+	static String MinimumAge;
+	static String MaximumAge;
+	static String AppliedFor;
+	static boolean Vegetarian;
+	static String OrganicType;
+	static String Ingredient;
+	static String Flovor;
+	static double price;
+	static int quantity;
+	static boolean available;
+	static double ratings;
+	static String Shape;
+	static boolean helpProtect;
+	static String Suitable;
+	static int Pack; 
+	static String Variants;
+	static String  color;
+	
+	static void setcompanyName(String value)
+	{
+		companyName=value;
+	}
+	
+	static void setProductHighlights(String value)
+	{
+		ProductHighlights=value;
+	}
+	
+	static void setModelName(String value)
+	{
+		ModelName=value;
+	}
+	
+	static void setIdeal_For(String value)
+	{
+		Ideal_For=value;
+	}
+	
+	static void setMinimumAge(String value)
+	{
+		MinimumAge=value;
+	}
+	
+	static void setMaximumAge(String value)
+	{
+		MaximumAge=value;
+	}
+	
+	static void setAppliedFor(String value)
+	{
+		AppliedFor=value;
+	}
+	
+	static void setVegetarian(boolean value)
+	{
+		Vegetarian=value;
+	}
+	
+	static void setOrganicType(String value)
+	{
+		OrganicType=value;
+	}
+	
+	static void setIngredient(String value)
+	{
+		Ingredient=value;
+	}
+	
+	static void setFlovor(String value)
+	{
+		Flovor=value;
+	}
+	
+	static void setprice(double value)
+	{
+		 price=value;
+	}
+	
+	static void setquantity(int value)
+	{
+		quantity=value;
+	}
+	
+	static void setavailable( boolean value)
+	{
+		available=value;
+	}
+	
+	static void setratings(double value)
+	{
+		ratings=value;
+	}
+	
+	static void setShape(String value)
+	{
+		Shape=value;
+	}
+	
+	static void sethelpProtect(boolean value)
+	{
+		helpProtect=value;
+	}
+	
+	static void Suitable(String value)
+	{
+		Suitable=value;
+	}
+	
+	static void setPack(int value)
+	{
+		Pack=value;
+	}                                                  
+	
+	static void Variants(String value)
+	{
+		Variants=value;
+	}
+	
+	static void color(String value)
+	{
+		color=value;
+	}
+}
